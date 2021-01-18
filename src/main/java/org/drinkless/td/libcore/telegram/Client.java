60feb902f6222f37edaea41b3798feea66748016
vkryl <vkryl@users.noreply.github.com>
2021-01-18 07:49:58 +0400

    fixed copyright

--- src/main/java/org/drinkless/td/libcore/telegram/Client.java
+++ src/main/java/org/drinkless/td/libcore/telegram/Client.java
@@ -1,21 +1,8 @@
 /*
- * This file is part of TD.
+ * Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2021
  *
- * TD is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * TD is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with TD.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright 2014-2019 Arseny Smirnov
- *           2014-2019 Aliaksei Levin
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
 package org.drinkless.td.libcore.telegram;
