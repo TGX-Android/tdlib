ab55685978ac2a7fb9488b35bba7843779dda465
vkryl <6242627+vkryl@users.noreply.github.com>
2023-05-27 18:37:35 +0400

    Moved namespace declaration to `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+
 plugins {
     id("com.android.library")
     id("module-plugin")
@@ -11,4 +12,8 @@ plugins {
 
 dependencies {
     implementation("androidx.annotation:annotation:${LibraryVersions.ANNOTATIONS}")
+}
+
+android {
+  namespace = "org.drinkless.td.libcore"
 }
