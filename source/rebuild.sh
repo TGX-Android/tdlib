5c7a7b466efd93af4953b79314dfc38d268941ae
vkryl <6242627+vkryl@users.noreply.github.com>
2026-08-31 15:37:40 +0400

    Add ability to pass output dir

--- source/rebuild.sh
+++ source/rebuild.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
+SYMBOLS_INSTALL_DIR=${1:-~/tdlib-symbols}
+
 rm -rf build
 
 ./build-openssl.sh || (echo "OpenSSL build failed" && exit 1)
 ./build-tdlib.sh || (echo "TDLib build failed" && exit 1)
 
-rm -rf ~/tdlib-symbols
-./install.sh ~/tdlib-symbols
-
-./clean.sh
+rm -rf "${SYMBOLS_INSTALL_DIR:?}/*"
+mkdir -p "${SYMBOLS_INSTALL_DIR:?}"
+./install.sh "${SYMBOLS_INSTALL_DIR:?}"
