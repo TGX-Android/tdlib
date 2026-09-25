2f5b7621998be1ab7ea9d786cc672b2892f3a25b
vkryl <6242627+vkryl@users.noreply.github.com>
2026-09-26 02:18:12 +0400

    Remove build files with each checkout

--- source/clean.sh
+++ source/clean.sh
@@ -2,15 +2,15 @@
 set -e
 
 pushd openssl || exit 1
-git clean -ffd
-git submodule foreach -q --recursive 'git clean -ffd'
+git clean -ffdx
+git submodule foreach -q --recursive 'git clean -ffdx'
 git reset --hard HEAD
 git submodule update --init
 popd > /dev/null
 
 pushd td || exit 1
-git clean -ffd
-git submodule foreach -q --recursive 'git clean -ffd'
+git clean -ffdx
+git submodule foreach -q --recursive 'git clean -ffdx'
 git reset --hard HEAD
 git submodule update --init
 popd > /dev/null
